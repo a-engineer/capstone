84477637bdf5adfa1692614f0096d8dfa4174767
a-engineer <72283706+a-engineer@users.noreply.github.com>
2022-05-27 00:02:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,31 +5,6 @@ import streamlit as st
 import pandas as pd
 import streamlit_authenticator as stauth
 
-
-#---------USER AUTHENTICATION------------------
-names = ["Peter Parker", "Rebecca Miller"]
-usernames = ["pparker", "rmiller"]
-
-# Load hashed passwords
-file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("rb") as file:
-	hashed_passwords = pickle.load(file)
-
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "capstone", "abcdef", cookie_expiry_days=0)
-
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status == False:
-	st.error("Username/password is incorrect")
-
-if authentication_status == None:
-	st.warning("Please enter your username and password")
-
-if authentication_status:
-
-	#authenticator.logout("Logout", "sidebar")
-	#st.sidebar.title(f"Welcome {name}")
-	#st.sidebar.header("Logout Here")
 	header  = st.container()
 	dataset = st.container()
 	features = st.container()
