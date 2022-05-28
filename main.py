934e77117d1b0abed63b19c7fbe8c284a3acfd3d
a-engineer <72283706+a-engineer@users.noreply.github.com>
2022-05-27 23:26:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,30 @@ import pandas as pd
 import streamlit as st
 import streamlit_authenticator as stauth
 
-header  = st.container()
-dataset = st.container()
-features = st.container()
-modelTraining = st.container()
+#---------Password Login------------
+names = ['John Smith','Rebecca Briggs']
+usernames = ['jsmith','rbriggs']
+passwords = ['123','456']
 
-with header:
-	st.markdown("<h1 style='text-align: center; color: black;'>Welcome to my Data Science Capstone!</h1>", unsafe_allow_html=True)
+hashed_passwords = stauth.Hasher(passwords).generate()
 
-	#st.title('Welcome to my Data Science Capstone!')
-	st.text('''In this project, I will take historical employee productivity data
-and create a multiple linear regression model to be able to
-predict employee productivity.
-		''')
+authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login('Login','sidebar')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write('Welcome *%s*' % (name))
+    header  = st.container()
+    dataset = st.container()
+    features = st.container()
+    modelTraining = st.container()
+    with header: st.markdown("<h1 style='text-align: center; color: black;'>Welcome to my Data Science Capstone!</h1>", unsafe_allow_html=True)
+    with header: st.text('''In this project, I will take historical employee productivity data and create a 
+multiple linear regression model to be able to predict employee productivity.''')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
