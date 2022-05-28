bb6c631158b0afddb0d4699a1e756ddcd31c54b2
a-engineer <72283706+a-engineer@users.noreply.github.com>
2022-05-27 22:56:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+
+
+header  = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header:
+	st.markdown("<h1 style='text-align: center; color: black;'>Welcome to my Data Science Capstone!</h1>", unsafe_allow_html=True)
+
+	#st.title('Welcome to my Data Science Capstone!')
+	st.text('''In this project, I will take historical employee productivity data
+and create a multiple linear regression model to be able to
+predict employee productivity.
+		''')
