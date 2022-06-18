be6e6f6b214bab29bf24e7afb7b8225309df7fd0
a-engineer <72283706+a-engineer@users.noreply.github.com>
2022-06-18 03:49:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -45,25 +45,27 @@ with dataset:
 
 st.subheader('Choose a file to upload or Select Example Dataset Button.')
 
-uploaded_file = st.file_uploader("Choose a file") 
-if uploaded_file is not None:       
-    # Cache dataset
-    @st.cache
-    def get_data():
-        # Reads an uploaded file
-        df = pd.read_csv(uploaded_file)
-        return df
-    
-    # Displays the first five rows
-    df = get_data()
-    st.subheader('First five rows of data.')
-    st.table(df.head())
-
 if st.button("Example Dataset"):
     df = pd.read_csv("employee productivity v2.csv") 
     # Displays the first five rows
     st.subheader('First five rows of data.')
     st.table(df.head())
+
+else:
+    uploaded_file = st.file_uploader("Choose a file") 
+    if uploaded_file is not None:       
+        # Cache dataset
+        @st.cache
+        def get_data():
+            # Reads an uploaded file
+            df = pd.read_csv(uploaded_file)
+            return df
+        
+        # Displays the first five rows
+        df = get_data()
+        st.subheader('First five rows of data.')
+        st.table(df.head())
+
 
 
 
