baa04adcd6b4dab0b4eb2f608fa4ff88d4e69be6
a-engineer <72283706+a-engineer@users.noreply.github.com>
2022-06-18 03:16:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ st.subheader('Choose a file to upload.')
     uploaded_file = st.file_uploader("Choose a file") 
     if uploaded_file is not None:
         
-# Cache dataset
-@st.cache
-def get_data():
-    # Reads an uploaded file
-    df = pd.read_csv(uploaded_file)
-    return df
+        # Cache dataset
+        @st.cache
+        def get_data():
+            # Reads an uploaded file
+            df = pd.read_csv(uploaded_file)
+            return df
