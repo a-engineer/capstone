0d68d6daa62ce908433d505dbb626d81448dbc3b
a-engineer <72283706+a-engineer@users.noreply.github.com>
2022-06-18 03:33:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ st.subheader('Choose a file to upload or Select Example Dataset Button.')
 
 if st.button("Example Dataset"):
     df = pd.read_csv("employee productivity v2.csv") 
+    # Displays the first five rows
+    st.subheader('Uploaded files first five rows of data.')
+    st.table(df.head())
 
 uploaded_file = st.file_uploader("Choose a file") 
 #if uploaded_file is not None:
 
-# Displays the first five rows
-st.subheader('Uploaded files first five rows of data.')
-st.table(df.head())
